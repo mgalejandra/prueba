c1c21cc7339c76128202d08aa716d3373593dee8
Alejandra Gonzalez <migdualy@gmail.com>
2023-01-20 12:02:04 -0400

    docu

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Chinchin'
+copyright = '2023, https://www.pagochinchin.com/'
+author = 'Chinchin'
 
 release = '0.1'
 version = '0.1.0'
